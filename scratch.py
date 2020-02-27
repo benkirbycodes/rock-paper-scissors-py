e6ddfa3af76bdaac53e9ac5ad965a3040e9fab8f
benkirbycodes <benkirbycodes@gmail.com>
2020-02-27 15:56:58 -0700

    Setup Dictionary and Random Generator

--- scratch.py
+++ scratch.py
@@ -1,5 +1,17 @@
+# Rock Paper Scissors Console Game
+
+# Setup
+import random
+randomInt = random.randint(1, 3)
+
+choices = {"rock": 1, "paper": 2, "scissors": 3}
+
+# User Messages & Input
 
 print("ROCK PAPER SCISSORS")
 print("---------------------""\n")
 name = input("What is your name?  \n")
 print("Nice to meet you " + name + "\n")
+choice = input("Rock, Paper or Scissors?")
+
+choices[choice.lower()]
